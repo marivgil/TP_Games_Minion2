b597a4bc34a613173420837d2f36bbe8bdca89b2
marivgil <gil.maricruz@gmail.com>
2018-10-09 00:22:26 -0300

    el bicho salta

--- Minion2/KinematicBody2D.gd
+++ Minion2/KinematicBody2D.gd
@@ -1,18 +1,24 @@
 extends KinematicBody2D
 
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -500
 var motion = Vector2()
-var motionx = 0
-var motiony = 0
 
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -SPEED
 	else:
 		motion.x = 0
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
 
-	move_and_slide(motion)
+	motion = move_and_slide(motion, UP)
 	pass
