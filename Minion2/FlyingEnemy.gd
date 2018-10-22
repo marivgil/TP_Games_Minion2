d416eb22638bba0965d8ab7bd972075ceb023ae6
marivgil <gil.maricruz@gmail.com>
2018-10-21 21:32:55 -0300

    se agrega enemigo volador

--- Minion2/FlyingEnemy.gd
+++ Minion2/FlyingEnemy.gd
@@ -4,38 +4,28 @@ var flip = true
 var initial_position
 var final_position
 var velocity = 0.3
-var lifes = 2
-var firstCollision = true
-
 
 func _ready():
-	$AnimatedSprite.play("Idle")
+	$AnimatedSprite.play("default")
 	initial_position = $".".position.x
 	final_position = initial_position + 50
 	
 func _process(delta):
 	if(initial_position <= final_position and flip):
 		position.x += velocity
-		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.flip_h = false
 		if(position.x >= final_position):
 			flip = false
 			
 	if(position.x >= initial_position and !flip):
 		position.x -= velocity
-		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.flip_h = true
 		if(position.x <= initial_position):
 			flip = true
 			
 func hurt():
-	if(firstCollision):
-		firstCollision = false
-	else:
-		if(lifes==1):
-			queue_free()
-		elif(lifes==2):
-			$AnimatedSprite.play("gotHit")
-			velocity += 1
-			lifes -= 1
+	# este enemigo no se puede matar, decision de diseño del nivel
+	pass
 
 
 func _on_Area2D_body_entered(body):
